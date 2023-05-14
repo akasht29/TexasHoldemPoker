aee4270ece3419d8d422d984ca5decc48a5b650f
DevinLKern <devinkern@gmail.com>
2023-05-13 23:57:21 -0700

    navigation now updates backend

--- texasHold-web-app/backend/src/router/gamesRoutes.js
+++ texasHold-web-app/backend/src/router/gamesRoutes.js
@@ -1,25 +1,48 @@
 const express          = require('express');
 const router           = express.Router();
 const gameController   = require('../controllers/gameController');
-const { getAllGames } = require('../models/game/gameModel');
+const playerController = require('../controllers/playerController');
 
 router.get('/waiting-room/:gameId', async (request, response) => {
     try {
-        const gameId   = request.params.gameId;
-        const playerId = request.session.user;
+        const gameId = request.params.gameId;
+        const userId = request.session.user.user_id;
 
+        // check if there is room in the game
+        if (await gameController.gameFull(gameId)) {
+            response.redirect('/user/lobby');
+        }
+
+        
+
+        // generate a new player id for the user if needed
+        let playerId;
+        if (request.session?.player?.playerId) {
+            // if this block executes, it just means (at least in theory) 
+            // that the the player refreshed the page.
+            playerId = request.session.player.playerId;
+        }
+        else {
+            player = { 
+                playerId: await playerController.addPlayer(gameId, userId)
+            };
+            request.session.player = player;
+            playerId = player.playerId;
+        }
+        
+        console.log("playerData:", request.session.player, playerId);
         // check if the game has started.
         if (await gameController.gameStarted(gameId)) {
-            response.redirect(`/game/room/${gameId}`)
+            response.redirect(`/game/room/${gameId}`);
         }
 
         response.render('game-waiting-room', { 
             gameId: gameId, 
-            lobbyOwner: await gameController.firstPlayer(gameId) 
+            lobbyOwner: await gameController.firstPlayer(gameId, playerId) 
         });
     }
     catch (error) {
-        console.log("game room error:", error.message);
+        console.log("game waiting-room error:", error.message);
     }
 });
 
@@ -33,37 +56,39 @@ router.get('/room/:gameId/start', (request, response) => {
         response.render('game-room', { gameId: gameId });
     }
     catch (error) {
-        console.log("game room error:",)
+        console.log("game room start error:",)
     }
 });
 
-router.get('/room/:gameId/leave'), (request, response) => {
-    // TODO:
-    // Remove request.session.user from game gameId. 
-    response.redirect('/user/lobby');
-}
-
-router.get('/room/:gameId', (request, response) => {
+router.get('/room/:gameId', async (request, response) => {
     try {
         // TODO: 
         // Check if the player is in the game If there is room, add 
         // the player. Otherwise, redirect the player back to the lobby
 
-
         const gameId = request.params.gameId;
+
+        if (await gameController.gameFull(gameId)) {
+            redirect("user/lobby");
+        }
 
         response.render('game-room', { gameId: gameId });
     }
     catch (error) {
-        console.log("game room error:",)
+        console.log("game room error:", error.message);
     }
 });
 
-router.get('/room/:gameId/leave', (request, response) => {
-    const gameId   = request.params.gameId;
-    const playerId = request.session.user;
-
-    // gameController.removePlayer(gameId, playerId);
+router.get('/room/:gameId/leave', async (request, response) => {
+    console.log("SUPERDOUBLYGIGANTICMARKEREXTRAVAGANZA!!!!");
+    console.log("playerId:", JSON.stringify(request.session.player.playerId));
+    await playerController.removePlayer(
+        request.params.gameId, 
+        request.session.player.playerId
+    );
+    request.session.player = null;
+    console.log("SUPERDOUBLYGIGANTICMARKEREXTRAVAGANZA!!!!");
+    await gameController.deleteGameIfEmpty(request.params.gameId);
 
     response.redirect('/user/lobby');
 });
@@ -80,7 +105,7 @@ router.post('/create', async (request, response) => {
         if (!newGameInfo) {
             throw new Error("Could not make game");
         }
-      
+        
         response.redirect(`/game/waiting-room/${newGameInfo.game_id}`);
     }
     catch (error) {
